2803520fae84a7d6f5ad30e61aa85a308092ba5d
zhangzuorong <994255496@qq.com>
2019-05-13 10:45:02 +0800

    2.0

---
+++ src/main/java/com/zzr/springboot/controller/AopController.java
@@ -0,0 +1,34 @@
+package com.zzr.springboot.controller;
+
+import com.zzr.springboot.config.aop.Monitor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 开发公司：山东海豚数据技术有限公司
+ * 版权：山东海豚数据技术有限公司
+ * <p>
+ * AopController
+ *
+ * @author zzr
+ * @created Create Time: 2019/5/13
+ */
+@RestController
+@RequestMapping("/aop")
+public class AopController {
+
+    @GetMapping("index")
+    @ResponseBody
+    public String index(String userName){
+        return "你好："+userName;
+    }
+
+    @GetMapping("test")
+    @ResponseBody
+    @Monitor(text = "接口描述", value = "接口描述", title = "接口标题")
+    public String test(String userName){
+        return "你好："+userName;
+    }
+}
